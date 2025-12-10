cb21c4ad8bc23264122e6d6357c15115d2049516
aidafitria11 <aidafitria2004@gmail.com>
2025-12-10 12:03:13 +0700

    Progress 2 Menambahkan Sparepart Page

--- D'delfar/routes/web.php
+++ D'delfar/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('LandingPage');
 });
+
+Route::get('/sparepart', function () {
+    return view('user.Sparepart');
+})->name('sparepart');
